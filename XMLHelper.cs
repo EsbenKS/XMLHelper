c49a5ebc0104d3e24115e6893a24b9c1db55756c
EsbenKS <33330790+EsbenKS@users.noreply.github.com>
2019-06-12 09:24:00 +0200

    Add files via upload

---
+++ XMLHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization; 
+
+namespace MasterItem.Shared
+{
+    public class XMLHelper
+    {
+
+        public static  ObjectType  ReadXML<ObjectType>(string filename)
+        {
+            using (var sw = new StreamReader(filename)))
+            {
+                return (ObjectType)new XmlSerializer(typeof(ObjectType)).Deserialize(sw);
+            }
+        }
+
+        public static void SaveXml<ObjectType>(ObjectType o, string filename)
+        {
+            using (var sw = new StreamWriter(filename))
+            {
+                new XmlSerializer(typeof(ObjectType)).Serialize(sw, o);
+            }
+        }
+    }
+}
